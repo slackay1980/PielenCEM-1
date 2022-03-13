fe2e991051754eb0675af093e21bbb8f1e1b09da
slackay1980 <slackay1980@gmail.com>
2022-03-13 14:17:32 +0100

    Angefangen mit DialogViews 13.03.2022

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,39 @@
+import controller.LoginController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Modality;
 import javafx.stage.Stage;
-import org.hibernate.Session;
+import controller.LoginController;
 
 public class Main extends Application {
 
+
+
     @Override
     public void start(Stage primaryStage) throws Exception{
+
+        //MainTest mainTest = new MainTest();
+        //HibernateUtil.getSessionFactory();
+
         Parent root = FXMLLoader.load(getClass().getResource("view/MainWindow.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("PielenCEM");
         primaryStage.setScene(new Scene(root, 1200, 775));
 
-        LoginDialog login = new LoginDialog(primaryStage);
-        login.initialize();
-        login.showAndWait();
+        LoginController loginController = new LoginController(primaryStage);
+        loginController.initialize();
+        loginController.showAndWait();
+
         primaryStage.show();
 
-        Session session = util.HibernateUtil.getSessionFactory().openSession();
+       // Session session = util.HibernateUtil.getSessionFactory().openSession();
 
     }
 
 
+
     public static void main(String[] args) {
-        launch(args);
+            launch(args);
 
     }
 }
